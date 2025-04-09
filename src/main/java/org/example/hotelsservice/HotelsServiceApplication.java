ec8a260acfefec4d69e3ebd8c20b425368696879
mrodvl <rodionov.mvg@gmail.com>
2025-04-09 22:28:32 +0300

    add commit

--- src/main/java/org/example/hotelsservice/HotelsServiceApplication.java
+++ src/main/java/org/example/hotelsservice/HotelsServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelsServiceApplication {
 
+    //Hi
     public static void main(String[] args) {
         SpringApplication.run(HotelsServiceApplication.class, args);
     }
