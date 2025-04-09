cca535db7eed33f50bed370c5f09c9039f695924
Snekold <e89030066143@yandex.ru>
2025-04-09 22:04:03 +0300

    add comment

--- src/main/java/org/example/hotelsservice/HotelsServiceApplication.java
+++ src/main/java/org/example/hotelsservice/HotelsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelsServiceApplication {
 
     //main
+    //Egor
     public static void main(String[] args) {
         SpringApplication.run(HotelsServiceApplication.class, args);
     }
