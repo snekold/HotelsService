e4dad4737bfe4436d396faf51ce9b1bc32efe2d1
MaksMusic <Maksmusic05@yandex.ru>
2025-04-10 13:02:51 +0300

    add comment

--- src/main/java/org/example/hotelsservice/HotelsServiceApplication.java
+++ src/main/java/org/example/hotelsservice/HotelsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelsServiceApplication {
 
     //main
+    //Maks
     public static void main(String[] args) {
         SpringApplication.run(HotelsServiceApplication.class, args);
     }
